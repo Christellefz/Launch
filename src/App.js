418177634559140eee298287790794ac58b87cc1
Christellefz <christellefz@yahoo.fr>
2021-11-15 16:12:37 +0100

    transfer de loading vers launch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import logo from './assets/logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img src={logo} alt='logo fz indev' />
+      <h1 className='tracking-in-expand'>Lancement du site</h1>
+      <h1 className='tracking-in-expand'>le 05/12/2021</h1>
+      <h2 className='bounce-in-top '>Une question?</h2>
+      <p className='bounce-in-top2'>
+        Contactez moi : 06-66-36-42-27 ou par Mail: contact@fzindev.fr
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
